7255362d55fcdbf642055070fdee464bef508d52
TranVinh <xuanvinhdn2003@gmail.com>
2024-04-24 14:57:40 +0200

    testScore0TousFrames

--- test/BowlingGame/BowlingGameTest.java
+++ test/BowlingGame/BowlingGameTest.java
@@ -2,22 +2,34 @@ package BowlingGame;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class BowlingGameTest {
 	BowlingGame game;
 
+	@BeforeEach
+	void setUp() {
+		game = new BowlingGame();
+	}
+	
 	@Test
 	void testRoll() {
-		BowlingGame game = new BowlingGame();
 		game.roll(0);
 	}
 
 	@Test
 	void testScore() {
-		BowlingGame game = new BowlingGame();
 		game.roll(0);
 		assertEquals(game.score(),0);
 	}
 
+	@Test
+	void testScore0TousFrames(){
+		for (int i = 0 ; i < 20 ; i++) {
+			game.roll(0);
+		}
+		assertEquals(game.score(),0);
+	}
+	
 }
